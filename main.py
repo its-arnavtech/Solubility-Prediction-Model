4bd58124b573177049a327c2785a8b994fb262cb
its-arnavtech <arnavk174@gmail.com>
2025-07-07 21:12:07 -0500

    random forest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -17,7 +16,6 @@ from sklearn.linear_model import LinearRegression
 
 lr = LinearRegression()
 lr.fit(x_train, y_train)
-print(lr)
 #print(x_train.dtypes)
 #print(y_train.dtypes)
 #print(x_train.apply(lambda col: col.astype(str).str.contains('SE').any()))
@@ -26,5 +24,17 @@ print(lr)
 
 y_lr_test_pred = lr.predict(x_test)
 y_lr_train_pred = lr.predict(x_train)
-print(y_lr_train_pred)
+#print(y_lr_train_pred)
 #model's built
+
+#random forest method
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor(max_depth=2, random_state=100)
+rf.fit(x_train, y_train)
+y_rf_test_pred = rf.predict(x_test)
+y_rf_train_pred = rf.predict(x_train)
+print(y_rf_train_pred)
+#Random Forest Regressor model is built
+
+#visualize model
+import numpy as np
